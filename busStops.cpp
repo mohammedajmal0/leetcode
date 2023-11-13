19a13dd104079fd1e7ae4b647992ab4cdd8474a6
mohammedajmal0 <mohammedajmal007.0@gmail.com>
2023-11-13 22:47:01 +0530

    solution template

--- busStops.cpp
+++ busStops.cpp
@@ -1,9 +1,37 @@
-class busStops
-{
+class Solution {
 public:
-    int busTravelled(){
-        
-        return 1;
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        unordered_map<int,vector<int>> busStopToBid;
+        for(int i=0;i<routes.size();i++){
+            int bid=i;
+            vector<int> busStops=routes[i];
+            for(auto busStop : busStops){
+                busStopToBid[busStop].push_back(bid);
+            }
+        }
+        queue<pair<int,int>> q; // current stop , bus changed
+        q.push({source,0});
+        unordered_map<int,bool> busVisited,busStopVisited;
+        busStopVisited[source]=true;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                pair<int,int> p=q.front();
+                q.pop();
+                int currStop=p.first;
+                int busTravelled=p.second;
+                if(currStop==target) return busTravelled;
+                for(auto bus: busStopToBid[currStop]){
+                    if(busVisited[bus]) continue;
+                    busVisited[bus]=true;
+                    for(auto stop : routes[bus]){
+                        if(busStopVisited[stop]) continue;
+                        busStopVisited[stop]=true;
+                        q.push({stop,busTravelled+1});
+                    }
+                }
+            }
+        }
+        return -1;    
     }
 };
-
