d02354f23cbb397f70ae9ed3e9b3c11603b80fd0
mohammedajmal0 <mohammedajmal007.0@gmail.com>
2023-11-16 21:57:55 +0530

    initial commit

---
+++ uniqueBinaryString.cpp
@@ -0,0 +1,14 @@
+// 1980. Find Unique Binary String
+// Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n=nums.size();
+        string ans="";
+        for(int i=0;i<n;i++){
+            char c=nums[i][i];
+            ans+= c=='1' ? '0' : '1';
+        }
+        return ans;
+    }
+};
